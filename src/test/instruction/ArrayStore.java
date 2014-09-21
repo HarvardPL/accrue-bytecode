d5203a30857b3777f88c8b004061ca1bf7e33a7a
Andrew Johnson <ajohnson@seas.harvard.edu>
2014-09-21 14:37:32 -0400

    Test for simple arraystore

--- src/test/instruction/ArrayStore.java
+++ src/test/instruction/ArrayStore.java
@@ -1,7 +1,9 @@
 package test.instruction;
 
 public class ArrayStore {
+    static int[] x = new int[4];
+    static int y;
     public static void main(String[] args) {
-
+        x[0] = y;
     }
 }
