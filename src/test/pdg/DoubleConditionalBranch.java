0ec4a4b4174ffa5885ac93f34b0132b64875c075
Andrew Johnson <ajohnson@seas.harvard.edu>
2014-09-28 16:22:08 -0400

    Improved test case

--- src/test/pdg/DoubleConditionalBranch.java
+++ src/test/pdg/DoubleConditionalBranch.java
@@ -2,26 +2,33 @@ package test.pdg;
 
 public class DoubleConditionalBranch {
     static boolean x;
+    static boolean a;
     static int y;
     static int z;
+    static int[] foo = new int[1];
+    static int[] foo2 = new int[1];
+    static int bar;
+    static int bar2;
 
     public static void main(String[] args) {
 
         if (x) {
-            y = 6;
-        }
-        else {
-            y = 7;
+            bar = foo.length;
         }
         z = 42;
 
-        if (x) {
-            y = 8;
+        if (a) {
+            bar2 = foo2.length;
         }
-        else {
-            y = 9;
-        }
-        z = 48;
+        y = 48;
+        //        if (a) {
+        //            bar = foo.length;
+        //            y = 8;
+        //        }
+        //        else {
+        //            y = 9;
+        //        }
+        //        z = 48;
 
     }
 }
