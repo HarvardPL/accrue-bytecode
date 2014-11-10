8d349d2d1810a0672d0cac150f7ad70a0d28fcf8
Andrew Johnson <ajohnson@seas.harvard.edu>
2014-11-10 17:14:20 -0500

    slight mod to run script for PLDI

--- bin/pldiWALA.sh
+++ bin/pldiWALA.sh
@@ -7,35 +7,35 @@ echo ""
 echo "TAX"
 for i in $(eval echo {1..$max}); do 
 sudo purge
-time bin/runAnalysis.sh -cp $PIDGIN/classes/test/       -e test.programs.tax.TaxFreeMain       -n pdg -haf "type(2,1)" -useSingleAllocForImmutableWrappers -testMode
+time bin/runAnalysis.sh -cp $PIDGIN/classes/test/       -e test.programs.tax.TaxFreeMain       -n pdg -haf "type(2,1)" -useSingleAllocForImmutableWrappers $1
 done
 
 echo ""
 echo "UPM"
 for i in $(eval echo {1..$max}); do 
 sudo purge
-time bin/runAnalysis.sh -cp data/upm.jar                -e com._17od.upm.Driver                -n pdg -haf "type(2,1)" -useSingleAllocForImmutableWrappers -useSingleAllocPerThrowableType -testMode
+time bin/runAnalysis.sh -cp data/upm.jar                -e com._17od.upm.Driver                -n pdg -haf "type(2,1)" -useSingleAllocForImmutableWrappers -useSingleAllocPerThrowableType $1
 done
 
 echo ""
 echo "FREECS"
 for i in $(eval echo {1..$max}); do 
 sudo purge
-time bin/runAnalysis.sh -cp data/freecs.jar             -e freecs.Driver                       -n pdg -haf "type(2,1)" -useSingleAllocForImmutableWrappers -useSingleAllocPerThrowableType -useSingleAllocForPrimitiveArrays -testMode
+time bin/runAnalysis.sh -cp data/freecs.jar             -e freecs.Driver                       -n pdg -haf "type(2,1)" -useSingleAllocForImmutableWrappers -useSingleAllocPerThrowableType -useSingleAllocForPrimitiveArrays $1
 done
 
 echo ""
 echo "CMS"
 for i in $(eval echo {1..$max}); do 
 sudo purge
-time bin/runAnalysis.sh -cp data/cms.jar                -e cms.Driver                          -n pdg -haf "type(2,1)" -useSingleAllocForImmutableWrappers -useSingleAllocPerThrowableType -useSingleAllocForPrimitiveArrays
+time bin/runAnalysis.sh -cp data/cms.jar                -e cms.Driver                          -n pdg -haf "type(2,1)" -useSingleAllocForImmutableWrappers -useSingleAllocPerThrowableType -useSingleAllocForPrimitiveArrays $1
 done
 
 echo ""
 echo "TOMCAT CVE_2011_0013 "
 for i in $(eval echo {1..$max}); do 
 sudo purge
-time bin/runAnalysis.sh -cp data/tomcat_7_trunk.jar     -e tomcat.Driver_CVE_2011_0013         -n pdg -haf "type(2,1) x CollectionsTypeSensitive(3,2) x StringBuilderFullObjSensitive(2)" -useSingleAllocForGenEx
+time bin/runAnalysis.sh -cp data/tomcat_7_trunk.jar     -e tomcat.Driver_CVE_2011_0013         -n pdg -haf "type(2,1) x CollectionsTypeSensitive(3,2) x StringBuilderFullObjSensitive(2)" -useSingleAllocForGenEx $1
 done
 
 # echo ""
