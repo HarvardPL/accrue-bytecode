b723f9347f6a8bbd8cd48b79d78c1edd1fc1a7e5
Andrew Johnson <ajohnson@seas.harvard.edu>
2015-01-20 22:51:25 -0500

    Make sure to reprocess queries when a relevant kill set has possibly changed.

--- src/analysis/pointer/engine/PointsToAnalysisMultiThreaded.java
+++ src/analysis/pointer/engine/PointsToAnalysisMultiThreaded.java
@@ -235,6 +235,8 @@ public class PointsToAnalysisMultiThreaded extends PointsToAnalysis {
             return;
         }
 
+        execService.g.ppReach.checkPointsToGraphDelta(changes);
+
         IntIterator iter = changes.domainIterator();
         while (iter.hasNext()) {
             int n = iter.next();
