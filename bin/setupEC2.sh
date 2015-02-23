33b656411e1c56b022a623bdea322486dbd3defd
Andrew Johnson <ajohnson@seas.harvard.edu>
2015-02-23 13:45:25 -0500

    edit amazonEC2 script

--- bin/setupEC2.sh
+++ bin/setupEC2.sh
@@ -5,7 +5,7 @@
 # 
 # 1. login 
 # ssh -i ~/.ssh/[KEYFILE_NAME].pem ec2-user@[DNS_NAME]
-# ## I aliased "ssh -i ~/.ssh/[KEYFILE_NAME].pem" to assh and use:
+# #### I aliased "ssh -i ~/.ssh/[KEYFILE_NAME].pem" to assh and use:
 # assh ec2-user@[DNS_NAME]
 #
 # 2. Mount the code
@@ -14,6 +14,8 @@
 # 3. Run this script (need to use "source" for JAVA_HOME to be set properly in this shell)
 # source /mnt/experiments/accrue-bytecode/bin/setupEC2.sh
 #
+# 4. Test the setup
+# ant clean; ant; ant test
 
 ##### 1-time setup (already done if you use my drive)
 # scp rt.jar
@@ -26,9 +28,6 @@
 # sudo mkdir /mnt/experiments; sudo mount /dev/xvdf /mnt/experiments
 
 sudo yum -y install git ant ant-junit emacs
-echo Exporting
 export JAVA_HOME=/usr/lib/jvm/java-1.7.0-openjdk-1.7.0.75.x86_64/
-echo exported
-
 cd /mnt/experiments/accrue-bytecode
 git pull
