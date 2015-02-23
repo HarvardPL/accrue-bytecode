0426abc5a1b4f7974ca29fbe648f67fe89c665c8
Andrew Johnson <ajohnson@seas.harvard.edu>
2015-02-23 13:42:54 -0500

    edit amazonEC2 script

--- bin/setupEC2.sh
+++ bin/setupEC2.sh
@@ -1,4 +1,21 @@
-##### 1-time setup
+#####
+# This file contains commands needed to set up a fresh Amazon EC2 instance
+#
+# The drive with the code is in Oregon region us-west-2a. Your instance should be here too.
+# 
+# 1. login 
+# ssh -i ~/.ssh/[KEYFILE_NAME].pem ec2-user@[DNS_NAME]
+# ## I aliased "ssh -i ~/.ssh/[KEYFILE_NAME].pem" to assh and use:
+# assh ec2-user@[DNS_NAME]
+#
+# 2. Mount the code
+# sudo mkdir /mnt/experiments; sudo mount /dev/xvdf /mnt/experiments
+#
+# 3. Run this script (need to use "source" for JAVA_HOME to be set properly in this shell)
+# source /mnt/experiments/accrue-bytecode/bin/setupEC2.sh
+#
+
+##### 1-time setup (already done if you use my drive)
 # scp rt.jar
 # change path to JRE in to the path to rt.jar data/wala.properties
 
@@ -14,3 +31,4 @@ export JAVA_HOME=/usr/lib/jvm/java-1.7.0-openjdk-1.7.0.75.x86_64/
 echo exported
 
 cd /mnt/experiments/accrue-bytecode
+git pull
