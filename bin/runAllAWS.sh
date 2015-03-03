5d23998257788ea4d998d45e155fff6eb1146748
Andrew Johnson <ajohnson@seas.harvard.edu>
2015-03-02 21:59:55 -0500

    Edit AWS runall script.

--- bin/runAllAWS.sh
+++ bin/runAllAWS.sh
@@ -1,9 +1,11 @@
+# bin/runAllAWS.sh 2>&1 | tee wala$(date +"%Y.%m.%d.%H.%M.%S").txt
+
 for BM in \
   antlr bloat eclipse fop hsqldb luindex lusearch pmd xalan #chart jython
 do \
 
 echo processing ${BM}...
 
-time bin/runAnalysis.sh -cp data/dacapo-2006-10-MR2.jar -e dacapo.${BM}.Main -n pointsto2 -useSingleAllocForImmutableWrappers -useSingleAllocForPrimitiveArrays -useSingleAllocPerThrowableType
+time bin/runAnalysis.sh -cp data/dacapo-2006-10-MR2.jar -e dacapo.${BM}.Main -n nonnull -useSingleAllocForImmutableWrappers -useSingleAllocForPrimitiveArrays -useSingleAllocPerThrowableType
 
 done
