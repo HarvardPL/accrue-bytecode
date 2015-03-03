bece0de8c8e09d801c9f6c448af968cbe0408575
Andrew Johnson <ajohnson@seas.harvard.edu>
2015-03-03 15:22:14 -0500

    Changing default heap abstraction factory to match the flow-sensitive one.

--- src/main/AccrueAnalysisOptions.java
+++ src/main/AccrueAnalysisOptions.java
@@ -215,7 +215,7 @@ public final class AccrueAnalysisOptions {
      * Heap abstraction factory definition
      */
     @Parameter(names = { "-haf", "-heapAbstractionFactory" }, validateWith = AccrueAnalysisOptions.HafValidator.class, description = "The HeapAbstractionFactory class defining how analysis contexts are created.")
-    private String hafString = "[type(2,1), scs(2)]";
+    private String hafString = "InstanceInitFullObjSensitive x type(2,1)";
     /**
      * {@link HeapAbstractionFactory} defining how analysis contexts are created
      */
