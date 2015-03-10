6f98924344182605d07a6b75bb173ad77927835c
Daniel King <daniel.zidan.king@gmail.com>
2015-03-09 20:16:34 -0400

    Teach `StringConstraintDelta` how to `toString`

--- src/analysis/pointer/graph/StringConstraintDelta.java
+++ src/analysis/pointer/graph/StringConstraintDelta.java
@@ -58,4 +58,9 @@ public class StringConstraintDelta {
         return this.svrs;
     }
 
+    @Override
+    public String toString() {
+        return "StringConstraintDelta [svrs=" + svrs + "]";
+    }
+
 }
