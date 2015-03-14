a96b8ffe120e5927bc3619404cbfe6345c4d59de
Andrew Johnson <ajohnson@seas.harvard.edu>
2015-03-13 22:38:45 -0400

    merging updated signatures from flow sensitive branch.

---
+++ src/signatures/library/java/util/regex/Pattern.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 1999, 2011, Oracle and/or its affiliates. All rights reserved.
+ * ORACLE PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+
+package signatures.library.java.util.regex;
+
+public final class Pattern {
+    @SuppressWarnings("unused")
+    private void normalize() {
+        // Intentionally blank
+    }
+}
